551879c5ff39be293102eb99a6c715d760e624c5
a914599611 <126036090+a914599611@users.noreply.github.com>
2023-02-22 02:40:37 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-from flask import Flask, jsonify, request, render_template
-from chatbot.bot import ChatBot
-from chatbot.message import Message
+from flask import Flask, request, jsonify
+import openai
+import os
 
 app = Flask(__name__)
-bot = ChatBot()
 
-@app.route("/")
+# 从环境变量中获取 OpenAI API 密钥
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    # 根路由返回欢迎消息
+    return '欢迎来到 ChatBot！'
 
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    message = Message(content=content, is_user=True)
-    bot_response = bot.get_response(content)
-    response = Message(content=str(bot_response), is_user=False)
-    return jsonify({"messages": [message.to_dict(), response.to_dict()]})
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    # 从请求数据中获取聊天消息
+    message = request.json['message']
+
+    # 调用 OpenAI API 生成响应
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=f'Q: {message}\nA:',
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # 从 API 响应中提取响应文本
+    response_text = response.choices[0].text.strip()
+
+    # 将响应作为 JSON 返回
+    return jsonify({'response': response_text})
